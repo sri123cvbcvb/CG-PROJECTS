448b7a7fd2e807c7db776d112710306141141ce7
sri123cvbcvb <sriramjeeva01@gmail.com>
2025-12-30 12:18:24 +0530

    OOPS Banner App UC1: Print OOPS to console

--- src/com/cg/oopsbanner/OOPSBannerApp.java
+++ src/com/cg/oopsbanner/OOPSBannerApp.java
@@ -1,4 +1,16 @@
 package com.cg.oopsbanner;
 
+/**
+ * OOPSBannerApp
+ *
+ * UC1: Print OOPS to Console
+ *
+ * This program prints the literal text "OOPS"
+ * to the console.
+ */
 public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
 }
